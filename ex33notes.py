d3a74718c53541bbc9ca32c123e8ed6b89092b68
rgraybeal <rgraybeal@fastmail.com>
2019-02-21 11:36:57 -0800

    updating ex33.py with function definition

---
+++ ex33notes.py
@@ -0,0 +1,36 @@
+"""
+
+Ex  33 and using GitHub
+
+While loops
+
+
+while-loops keep looping as long as a boolean expression is True
+
+Avoiding infinite Loops
+1. make sure that you use while- loops sparingly. Using a for-loop is better.
+2. Revew your while staemtns and make sure that hte boolean test will become False at some point
+3. When in doubt, print out your test variable at the top and bottom of the while-loop to see what it's doing
+
+-------------------------------------
+
+git commands:
+git add "FileName" -A is all, . is all as well
+git status
+git commit -m "Give it a message."
+git push
+git pull to take off git and place on computer
+-----------------------------------------------
+ESC : wq to get out of a full-blown commit
+
+
+
+Workflow
+1. at beginning of day, start with git pull
+2. make your changes, git Add -A
+3. git commit -m "made mortar more specific"
+Multiple times a day
+
+Level up tutorial - install a git package that allows git commands in the text edit window
+
+"""
